7c1baaef1426f29441b5c6d7e9c19f275cd701d4
Blake Greeley <98073180+BlakeGreeley@users.noreply.github.com>
2022-05-05 15:12:33 -0700

    Add files via upload

--- CafeJava.java
+++ CafeJava.java
@@ -26,11 +26,39 @@ public class CafeJava {
         // Order completions (add yours below)
         boolean isReadyOrder1 = false;
         boolean isReadyOrder2 = true;
-    
-        // APP INTERACTION SIMULATION (Add your code for the challenges below)
-        // Example:
-        System.out.println(generalGreeting + customer1);
-        System.out.println(customer1 + readyMessage); // Displays "Welcome to Cafe Java, Cindhuri"
-    	// ** Your customer interaction print statements will go here ** //
+        boolean isReadyOrder3 = false;
+        boolean isReadyOrder4 = true;
+        
+        System.out.println(generalGreeting + customer1); // Displays "Welcome to Cafe Java, Cindhuri"
+        
+        // 1.) Cindhuri ordered a coffee. 
+        // show her the status message.
+        System.out.println(customer1 + pendingMessage);
+
+        // 2.) Noah ordered a cappucino
+        // Check the status of his order and let him know. If it is ready, also tell him his total.
+
+        if (isReadyOrder4) {
+            System.out.println(customer4 + readyMessage);
+            System.out.println(displayTotalMessage + cappucino);
+        }
+        else {
+            System.out.println(customer4 + pendingMessage);
+        }
+        
+        // 3.) Sam ordered 2 lattes, and would like to know the total up front. 
+        // Also tell them their order status.
+        System.out.println(displayTotalMessage + latte * 2);
+        if (isReadyOrder2) {
+            System.out.println(customer2 + readyMessage);
+        }
+        else {
+            System.out.println(customer2 + pendingMessage);
+        }
+
+
+        // 4.) Jimmy just realized he was charged for a coffee but had ordered a latte.
+        // Tell him his new total to make up the difference.
+        System.out.println(displayTotalMessage + (latte - dripCoffee));
     }
 }
